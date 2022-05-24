7047ded7da9f95f2d18128ec17917a671c6ea7c2
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2022-05-24 16:54:03 -0700

    game itination works now
    Co-authored-by: Ryan <Barely-Awake@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import config from './utils/readConfig.js';
 import commandAdder from './bot/commandAdder.js';
 import eventHandler from './bot/eventHandler.js';
 
-const intents = new Discord.Intents();
-intents.add(Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES);
+// all intents fuck the pricintpal of least permisisons
+const intents = new Discord.Intents(32767)
 
 const client = new Discord.Client({intents: intents});
 const clientCollections = {
---
+++ src/utils/getUserFromMention.ts
@@ -0,0 +1,17 @@
+import * as Discord from 'discord.js';
+
+
+export default function getUserFromMention(client:Discord.Client, mention:string) {
+        if (!mention) return;
+    
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            mention = mention.slice(2, -1);
+    
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+    
+            return client.users.cache.get(mention);
+        }
+    }
+
