36bd1d8d1d53f950f0f60a1e600a8a2e96144cd9
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2022-05-24 17:22:35 -0700

    added shutdown handeler

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import * as Discord from 'discord.js';
 import config from './utils/readConfig.js';
 import commandAdder from './bot/commandAdder.js';
 import eventHandler from './bot/eventHandler.js';
-
+import shutdown from './utils/shutdown.js';
+import Logger from './utils/logger.js';
 // all intents fuck the pricintpal of least permisisons
 const intents = new Discord.Intents(32767)
 
@@ -17,3 +18,25 @@ commandAdder(clientCollections.commands);
 eventHandler(client);
 
 client.login(config.token);
+process.on('SIGINT', async function() {
+  Logger.warn("Caught interrupt signal, itinating graceful shutdown");
+
+  try{
+    await shutdown();
+    await client.destroy()
+    process.exit();
+  }
+  catch(err:any){
+    Logger.error(err);
+  }
+
+
+
+
+    
+
+
+
+     
+}
+);
