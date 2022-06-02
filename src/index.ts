2afab4169268b11c28479356fca6a66b62bd6f3e
TomAndJerry342 <minecraftpro102018@gmail.com>
2022-06-01 17:20:11 -0700

    change(bot): used module augmentation instead of commands collection

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,38 @@ import commandAdder from './bot/commandAdder.js';
 import eventHandler from './bot/eventHandler.js';
 import shutdown from './utils/shutdown.js';
 import Logger from './utils/logger.js';
+
+declare module 'discord.js' {
+  interface Client<Ready extends boolean = boolean> {
+      commands: any
+
+  }
+}
+
+
 // all intents fuck the pricintpal of least permisisons
 const intents = new Discord.Intents(32767)
 
 const client = new Discord.Client({intents: intents});
-const clientCollections = {
-  commands: new Discord.Collection(),
-};
+client.commands = new Discord.Collection();
 
-export { clientCollections as default };
 
-commandAdder(clientCollections.commands);
+export { client as default };
+
+commandAdder();
 eventHandler(client);
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
 
 client.login(config.token);
 process.on('SIGINT', async function() {
