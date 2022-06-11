ecd61e3054b54991dbabb5af8f22fb3855961ff7
TomAndJerry342 <minecraftpro102018@gmail.com>
2022-06-11 01:31:04 -0700

    implemented slash commands

---
+++ src/bot/commands/slash/_example.ts
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import client from "../../../index.js"
+let command = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+//@ts-ignore
+	async execute(interaction) {
+
+		return interaction.reply(`Pong! 🏓 \n Total Latency is ${interaction.createdTimestamp - Date.now()}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+	},
+};
+
+export default command
