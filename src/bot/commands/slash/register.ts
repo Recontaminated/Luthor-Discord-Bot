dd7afb80a64cc143e5312d508a5b063bfe5d7e62
TomAndJerry342 <minecraftpro102018@gmail.com>
2022-06-12 00:38:29 -0700

    Update register to update OR insert

--- src/bot/commands/slash/register.ts
+++ src/bot/commands/slash/register.ts
@@ -1,36 +1,43 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import client from "../../../index.js"
-import Logger from '../../../utils/logger.js';
-import usernameToUUID from '../../../utils/minecraftUsernameToUUID.js';
-import { Player } from '../../../utils/mongo/mongoManager.js';
-
+import { SlashCommandBuilder } from "@discordjs/builders";
+import client from "../../../index.js";
+import Logger from "../../../utils/logger.js";
+import usernameToUUID from "../../../utils/minecraftUsernameToUUID.js";
+import { Player } from "../../../utils/mongo/schemas/player.js";
 
 let command = {
-	data: new SlashCommandBuilder()
-		.setName('register')
-		.setDescription('Register your minecraft username with Luthor!')
-        .addStringOption(option =>
-            option.setName('username')
-                .setDescription('Your minecraft username')
-                .setRequired(true))
-                ,
-//@ts-ignore
-	async execute(interaction) {
-        const { options } = interaction
-        let username = options.get("username")
-        let mojang
-        try {
-            mojang = await usernameToUUID(username.value)
-        } catch (error) {
-            return interaction.reply("Invalid username!")
-        }
+  data: new SlashCommandBuilder()
+    .setName("register")
+    .setDescription("Register your minecraft username with Luthor!")
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription("Your minecraft username")
+        .setRequired(true)
+    ),
+  //@ts-ignore
+  async execute(interaction) {
+    const { options } = interaction;
+    let username = options.get("username");
+    let mojang;
+    try {
+      mojang = await usernameToUUID(username.value);
+    } catch (error) {
+      return interaction.reply("Invalid username!");
+    }
 
+    await Player.findOneAndUpdate(
+      { discordId: interaction.user.id },
+      {
+        discordId: interaction.user.id,
+        minecraft: { friendlyName: mojang.name, UUID: mojang.id },
+      },
+      {
+        upsert: true,
+      }
+    );
 
-        const player = new Player({minecraft:{friendlyName: mojang.name, UUID: mojang.id}, discordId: interaction.user.id})
-		await player.save()
-
-        return interaction.reply("Sucessfulyl registered!");
-	},
+    return interaction.reply("Sucessfulyl registered!");
+  },
 };
 
 export default command;
