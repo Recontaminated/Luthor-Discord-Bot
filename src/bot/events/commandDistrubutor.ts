5419b9e876c0e7c84d65766b69a5ca4a9138e043
TomAndJerry342 <minecraftpro102018@gmail.com>
2022-06-12 00:38:10 -0700

    refactor(command: )moved interaction distrubutor into command distrubutor

--- src/bot/events/commandDistrubutor.ts
+++ src/bot/events/commandDistrubutor.ts
@@ -1,7 +1,7 @@
 import clientCollections from '../../index.js';
 import * as Discord from 'discord.js';
 import config from '../../utils/readConfig.js';
-
+import client from '../../index.js'
 export default async function (message: Discord.Message) {
   commandHandler(message);
 }
@@ -16,14 +16,26 @@ function commandHandler(message: Discord.Message) {
   commandName = commandName.slice(config.prefix.length);
   const args = messageArray.slice(1);
 
-  const command = clientCollections.commands.get(commandName);
+  const command = clientCollections.commands.text.get(commandName);
 
   if (!command || typeof command !== 'function')
     return;
 
   command(message, args);
 }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	const command = client.commands.slash.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		await command.default.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 export const settings = {
   once: false,
   event: "messageCreate"
