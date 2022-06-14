0bb37bbae57e7ae215416bb8f2601d54d5333cd4
TomAndJerry342 <minecraftpro102018@gmail.com>
2022-06-14 00:08:19 -0700

    Improved checks for register - checking for already regiseter instead of updating name just check if exists

--- src/bot/commands/slash/register.ts
+++ src/bot/commands/slash/register.ts
@@ -3,6 +3,7 @@ import client from "../../../index.js";
 import Logger from "../../../utils/logger.js";
 import usernameToUUID from "../../../utils/minecraftUsernameToUUID.js";
 import { Player } from "../../../utils/mongo/schemas/player.js";
+import errorBuilder from "../../responces/error.js";
 
 let command = {
   data: new SlashCommandBuilder()
@@ -17,22 +18,24 @@ let command = {
   //@ts-ignore
   async execute(interaction) {
     const { options } = interaction;
+    const existingDiscordData = await Player.find({discordId: interaction.user.id});
+
+    if (existingDiscordData.length !== 0)
+      return interaction.reply(errorBuilder('You have already linked on this discord account. Please use the unlink command and then try ' +
+        'linking again.', command.data.name, interaction.message));
+
     let username = options.get("username");
     let mojang;
     try {
       mojang = await usernameToUUID(username.value);
     } catch (error) {
-      return interaction.reply("Invalid username!");
+      return interaction.reply(errorBuilder(error=`Couldn't find user ${username.value}`, "register", interaction.message));
     }
 
-    await Player.findOneAndUpdate(
-      { discordId: interaction.user.id },
+    await Player.create(
       {
         discordId: interaction.user.id,
         minecraft: { friendlyName: mojang.name, UUID: mojang.id },
-      },
-      {
-        upsert: true,
       }
     );
 
