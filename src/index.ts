98e6a511370c3c8084ee40fd2e54778506c34fd9
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2022-06-15 17:44:43 -0700

    start moving config under client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Discord from "discord.js";
 import config from "./utils/readConfig.js";
+import { configType } from "./utils/readConfig.js";
 import commandAdder from "./bot/commandAdder.js";
 import eventHandler from "./bot/eventHandler.js";
 import shutdown from "./utils/shutdown.js";
@@ -9,6 +10,7 @@ import deployCommands from "./utils/deployCommands.js";
 import { Guild } from "./utils/mongo/schemas/guild.js";
 declare module "discord.js" {
   interface Client<Ready extends boolean = boolean> {
+    config:configType
     commands: any;
     prefix: any;
   }
@@ -18,12 +20,12 @@ declare module "discord.js" {
 const intents = new Discord.Intents(32767);
 
 const client = new Discord.Client({ intents: intents });
+client.config = config
 client.commands = {}
 
 client.commands.text = new Discord.Collection();
 client.commands.slash = new Discord.Collection();
 client.prefix = {}
-
 export { client as default };
 
 
@@ -39,11 +41,12 @@ let sycFunctions = async () => {
   Logger.info("Sync init functions");
   await eventHandler(client);
   await commandAdder();
-  await deployCommands()
   await initMongo();
   await loadPrefixes();
+  await deployCommands()
   Logger.info("Siginaling for async init");
   client.emit("asyncInit")
+  
 };
 sycFunctions();
 
