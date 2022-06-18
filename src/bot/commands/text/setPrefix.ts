33934819ae39f66658a470988819c28693d1de43
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2022-06-18 15:15:25 +0200

    remove unnesessary arguement in error builder

--- src/bot/commands/text/setPrefix.ts
+++ src/bot/commands/text/setPrefix.ts
@@ -3,15 +3,15 @@ import { Guild } from '../../../utils/mongo/schemas/guild.js';
 import errorBuilder from '../../responces/error.js';
 import client from '../../../index.js';
 export default async function (message: Discord.Message, args: string[]) {
-    if (!message.guild) return await message.channel.send(errorBuilder("You can't use this command in DM! Please ping me instead", "setprefix", message));
+    if (!message.guild) return await message.channel.send(errorBuilder("You can't use this command in DM! Please ping me instead", "setprefix"));
     let newPrefix = args[0];
     if (!newPrefix)
         return await message.channel.send(
-            errorBuilder('Please provide a prefix!', 'setPrefix', message)
+            errorBuilder('Please provide a prefix!', 'setPrefix')
         );
     if (newPrefix.length > 5)
         return await message.channel.send(
-            errorBuilder('Prefix must be less than 5 characters!','setPrefix',message)
+            errorBuilder('Prefix must be less than 5 characters!','setPrefix')
         );
     if (newPrefix.length > 1){
         await message.channel.send({embeds:[new Discord.MessageEmbed().setDescription(`Warning, prefix will be set with a trailing space. Ex: ${newPrefix} <Command>`).setColor("#ff0000")]});
