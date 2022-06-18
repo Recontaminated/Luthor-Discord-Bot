a20c22e5ac5699044487de5b9c148cfedbe9d835
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2022-06-18 23:01:08 +0200

    make remove chat channel actually work

--- src/bot/commands/text/cogHelpers/removeLuthorChatbotChannel.ts
+++ src/bot/commands/text/cogHelpers/removeLuthorChatbotChannel.ts
@@ -5,6 +5,7 @@ import errorBuilder from '../../../responces/error.js';
 export default async function (message: Discord.Message, args: string[]) {
    const guild = await Guild.findOne({ guildId: message.guild?.id });
    if (guild.features.luthorChatChannelId) {await Guild.findOneAndUpdate({ guildId: message.guild?.id },{features:{luthorChatChannelId:null}} )
+  client.cache.cog.luthorChatBot[message.guild.id] = null;
   await message.channel.send("Successfully removed chatbot channel!");
   }
   else {
