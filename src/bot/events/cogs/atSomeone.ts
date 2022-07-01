9ad862ce120d7299c763992c9ffbc3be24914404
TomAndJerry342 <minecraftpro102018@gmail.com>
2022-07-01 02:02:37 +0200

    feat(atsomeone:) atSomeone from that one discord video meme

---
+++ src/bot/events/cogs/atSomeone.ts
@@ -0,0 +1,31 @@
+import { Message } from "discord.js";
+import Logger from "../../../utils/logger.js";
+
+export default async function (message: Message) {
+    if (!message.content.toLowerCase().includes("@someone")) return;
+
+    const members = message.guild.members.cache;
+
+    let randomMember = members.random();
+
+    let loop = 0
+
+    while (true){
+        randomMember = members.random();
+        if (randomMember.user.bot) continue
+        if (!randomMember.presence) continue
+        if (randomMember.presence.status == "offline" ) continue
+        break
+
+
+
+    }
+
+
+    await message.channel.send(`${randomMember.user.toString()}`);
+}
+
+export const settings = {
+    once: false,
+    event: "messageCreate",
+};
