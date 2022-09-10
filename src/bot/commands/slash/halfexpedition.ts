e86971b90d4f8644bc336439c70d27ea1e35d719
Recontaminated <minecraftpro102018@gmail.com>
2022-09-09 18:14:52 -0700

    lmafo im so done

---
+++ src/bot/commands/slash/halfexpedition.ts
@@ -0,0 +1,36 @@
+import prettyMilliseconds from "pretty-ms";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import client from "index.js";
+import {AttachmentBuilder} from "discord.js";
+import fetch from 'node-fetch';
+let command = {
+    data: new SlashCommandBuilder()
+        .setName("halfexpedition")
+        .setDescription("makes a shitty image")
+        .addStringOption((option) =>
+            option
+                .setName("prompt")
+                .setDescription("Your prmopt idk")
+                .setRequired(true)
+        ),
+    //@ts-ignore
+    async execute(interaction) {
+        //make a post request to the api
+        const api = `http://192.168.1.88:5000/?username=${interaction.options.getString("prompt")}`;
+        let reply = await interaction.deferReply("hang on a sec")
+        const response = await fetch(api);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const attachment = new AttachmentBuilder(buffer, {name: interaction.options.getString("prompt") + ".png"});
+        return await interaction.editReply({files: [attachment]});
+
+        //response is a JPEG image
+
+        //send the image
+
+
+
+    },
+};
+
+export default command;
