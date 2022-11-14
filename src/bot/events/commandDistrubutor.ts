367f42f34f1169ffef14b80f4bfe9602ff7f2d88
Recontaminated <69064548+Recontaminated@users.noreply.github.com>
2022-11-14 15:13:10 -0800

    feat(commands): made it possible to use bot mention as a prefix on top of all other prefixes

--- src/bot/events/commandDistrubutor.ts
+++ src/bot/events/commandDistrubutor.ts
@@ -2,7 +2,6 @@ import * as Discord from "discord.js";
 import client from "../../index.js";
 import { InteractionType } from "discord.js";
 import Logger from "../../utils/logger.js";
-import { type } from "os";
 
 export default async function (message: Discord.Message) {
   commandHandler(message);
@@ -16,24 +15,25 @@ async function commandHandler(message: Discord.Message) {
   } else {
     prefix = client.prefix[message.guildId] || client.config.prefix;
   }
-
+  // if the command starts with mentioning the bot
+  if (message.content.startsWith(`<@${client.user.id}> `)) {
+    prefix = `<@${client.user.id}> `;
+  }
   if (!message.content.startsWith(prefix)) {
     return;
   }
+
 
   const commandMessage = message.content.slice(prefix.length);
   const messageArray = commandMessage.split(" ");
   const commandName = messageArray[0].toLowerCase();
 
   const args = messageArray.slice(1);
-
   let command = client.commands.text.get(commandName);
-  Logger.debug("Command: " + command.name);
   if (!command) {
     return;
   }
   command = command.run;
-  Logger.debug(command);
   if (!command || typeof command !== "function") {
     return;
   }
